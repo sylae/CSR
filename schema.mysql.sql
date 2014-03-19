ef8be3708febb58443d1e0a984cc535cc51a33e9
Sylae Corell <sylae@calref.net>
2014-03-19 00:41:14 -0600

    added 'composite' SQL view
    does a lot of the heavy work, hopefully removing the load from php

--- schema.mysql.sql
+++ schema.mysql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: iridium.local
--- Generation Time: Mar 17, 2014 at 09:29 PM
+-- Generation Time: Mar 19, 2014 at 12:31 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.4.4-14+deb7u5
 
@@ -14,6 +14,20 @@ SET time_zone = "+00:00";
 -- Database: `csr`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `composite`
+--
+CREATE TABLE IF NOT EXISTS `composite` (
+`tid` int(11) unsigned
+,`title` varchar(255)
+,`B` decimal(23,0)
+,`G` decimal(23,0)
+,`A` decimal(23,0)
+,`S` decimal(23,0)
+,`P` decimal(23,0)
+);
 -- --------------------------------------------------------
 
 --
@@ -50,3 +64,12 @@ CREATE TABLE IF NOT EXISTS `topic` (
   `title` varchar(255) NOT NULL,
   PRIMARY KEY (`tid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `composite`
+--
+DROP TABLE IF EXISTS `composite`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`csr`@`%` SQL SECURITY DEFINER VIEW `composite` AS select `topic`.`tid` AS `tid`,`topic`.`title` AS `title`,sum((`post`.`rating` = 5)) AS `B`,sum((`post`.`rating` = 4)) AS `G`,sum((`post`.`rating` = 3)) AS `A`,sum((`post`.`rating` = 2)) AS `S`,sum((`post`.`rating` = 1)) AS `P` from (`topic` join `post` on((`post`.`tid` = `topic`.`tid`))) group by `topic`.`tid`;
