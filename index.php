a53d65a921d6c8ca796afe375752152929c490c8
Sylae Corell <sylae@calref.net>
2014-06-09 23:04:42 -0600

    index.php, a sandbox for now

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Class inclusions and things to do!
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License 3
+ * @author Sylae Jiendra Corell <sylae@calref.net>
+ */
+require 'config.php';
+require 'qp.php';
+require 'HTTP/Request2.php';
+require 'MDB2.php';
+
+require 'CSR.php';
+require 'scenario.php';
+require 'scenPoll.php';
+require 'IPB.php';
+
+// Currently a sandbox for testing.
+// TODO: Iterate over things, or something.
+$scen = 12217;
+$poll = new scenPoll($scen);
+$csr = new scenario($scen);
+$csr->updateOP();
