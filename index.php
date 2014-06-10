71f63e31d37aa2165c6d54ba0062ebf0888147b9
Sylae Corell <sylae@calref.net>
2014-06-10 17:04:28 -0600

    tyop

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License 3
  * @author Sylae Jiendra Corell <sylae@calref.net>
  */
-require 'clases.php';
+require 'classes.php';
 
 // Currently a sandbox for testing.
 // TODO: Iterate over things, or something.
