bef182c20cb0b25cae3ac0743dad97fb6e73d2d3
Sylae Corell <sylae@calref.net>
2015-02-04 11:37:22 -0700

    add a function to build a BGASP chart out of progress bars

--- classes/webpage.php
+++ classes/webpage.php
@@ -29,16 +29,34 @@ class webpage extends CSR {
     if ($string) {
       $this->title = $string . " - BoA CSR";
     } else {
-      $this->title =  "BoA CSR";
+      $this->title = "BoA CSR";
     }
   }
-  
+
   protected function authToString($array) {
     $s = array();
     foreach ($array as $aid => $name) {
-      $s[] = '<a href="./?auth='.$aid.'">'.$name.'</a>';
+      $s[] = '<a href="./?auth=' . $aid . '">' . $name . '</a>';
     }
     return implode(", ", $s);
+  }
+
+  protected function buildBGBar($bgasp) {
+    $pct = array();
+    $tot = array_sum($bgasp);
+    foreach ($bgasp as $cat => $num) {
+      $pct[$cat] = $num / $tot * 100;
+    }
+    $bgaspbar = <<<EOT
+<div class="progress">
+<div class="progress-bar" style="width:{$pct[P]}%;background-color:#c00000;color:#333">{$bgasp[P]}</div>
+<div class="progress-bar" style="width:{$pct[S]}%;background-color:#ed7d31;color:#333">{$bgasp[S]}</div>
+<div class="progress-bar" style="width:{$pct[A]}%;background-color:#ffc000;color:#333">{$bgasp[A]}</div>
+<div class="progress-bar" style="width:{$pct[G]}%;background-color:#92d050;color:#333">{$bgasp[G]}</div>
+<div class="progress-bar" style="width:{$pct[B]}%;background-color:#00b0f0;color:#333">{$bgasp[B]}</div>
+</div>        
+EOT;
+    return $bgaspbar;
   }
 
 }
